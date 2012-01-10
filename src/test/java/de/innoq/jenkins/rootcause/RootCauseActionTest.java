9935f0151a7a6c495d993768d771c1862b80e31e
Martin Eigenbrodt <martin.eigenbrodt@innoq.com>
2012-01-10 17:06:45 +0100

    Added a unittest showing a diamond Problem in Root Cause Plugin

--- src/test/java/de/innoq/jenkins/rootcause/RootCauseActionTest.java
+++ src/test/java/de/innoq/jenkins/rootcause/RootCauseActionTest.java
@@ -1,26 +1,15 @@
 package de.innoq.jenkins.rootcause;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.TimeoutException;
-
-import hudson.Launcher;
-import hudson.model.BuildListener;
 import hudson.model.FreeStyleBuild;
-import hudson.model.TaskListener;
-import hudson.model.AbstractBuild;
+import hudson.model.Result;
 import hudson.model.Cause;
 import hudson.model.FreeStyleProject;
-import hudson.model.Hudson;
-import hudson.model.Run;
-import hudson.model.listeners.RunListener;
-import hudson.model.Result;
 import hudson.tasks.BuildTrigger;
-import hudson.util.OneShotEvent;
+
+import java.util.Collections;
+import java.util.List;
 
 import org.jvnet.hudson.test.HudsonTestCase;
-import org.jvnet.hudson.test.TestBuilder;
 
 import de.innoq.jenkins.rootcause.RootCauseAction.RootCause;
 
@@ -43,15 +32,7 @@ public class RootCauseActionTest extends HudsonTestCase {
 	public void testSimpleLinearRootCauseIsFound() throws Exception {
 		// Given projectA that triggers a Build of projectB
 
-		final OneShotEvent buildStarted = new OneShotEvent();
-		RunListener.LISTENERS.add(new RunListener<Run>() {
-
-			@Override
-			public void onCompleted(Run r, TaskListener listener) {
-				buildStarted.signal();
-			}
-
-		});
+	
 		FreeStyleProject projectA = createFreeStyleProject();
 		FreeStyleProject projectB = createFreeStyleProject();
 
@@ -66,8 +47,6 @@ public class RootCauseActionTest extends HudsonTestCase {
 		TestCause rootCause = new TestCause();
 		FreeStyleBuild build = projectA.scheduleBuild2(0, rootCause).get();
 		//
-		System.out.println("Blocking on buildStarted");
-		buildStarted.block();
 		System.out.println("Waiting for Project B to finish its build.");
 		while (projectB.getBuilds().isEmpty())
 			;
@@ -83,6 +62,60 @@ public class RootCauseActionTest extends HudsonTestCase {
 		System.out.println(result);
 	}
 
+	public void testDiamondDependencies() throws Exception {
+		
+			hudson.setNumExecutors(1);
+			// Given a diamond: A->B->D , A->C->D
+		
+			FreeStyleProject projectA = createFreeStyleProject("A");
+			FreeStyleProject projectB = createFreeStyleProject("B");
+			FreeStyleProject projectC = createFreeStyleProject("C");
+			FreeStyleProject projectD = createFreeStyleProject("D");
+
+			projectA.getPublishersList().add(
+					new BuildTrigger(Collections.singleton(projectB),
+							Result.SUCCESS));
+			
+			projectA.getPublishersList().add(
+					new BuildTrigger(Collections.singleton(projectC),
+							Result.SUCCESS));
+						
+			projectC.getPublishersList().add(
+					new BuildTrigger(Collections.singleton(projectD),
+							Result.SUCCESS));
+						
+			projectB.getPublishersList().add(
+					new BuildTrigger(Collections.singleton(projectD),
+							Result.SUCCESS));			
+			hudson.rebuildDependencyGraph();
+
+			projectD.getBuildWrappersList().add(new RootCauseBuildWrapper());
+
+			// When
+			TestCause rootCause = new TestCause();
+			FreeStyleBuild build = projectA.scheduleBuild2(0, rootCause).get();
+
+			
+			System.out.println("Waiting for Project D to finish its build.");
+			while (projectD.getBuilds().isEmpty())
+				;
+			build = projectD.getBuilds().get(0);
+			while (build.isBuilding())
+				;
+			System.out.println("Done Building");
+			RootCauseAction rootCauseAction = build
+					.getAction(RootCauseAction.class);
+			
+			//THEN 
+			
+			List<RootCause> result = rootCauseAction.getCauses();
+			// There should be only on root cause, we just triggered A
+			assertEquals(1, result.size());
+			assertEquals(rootCause, result.get(0).getCauseCount().keySet().iterator().next());
+			System.out.println(result);
+		
+		
+	}
 	public static class TestCause extends Cause {
 
 		@Override
